dd55fb1b308a8b4241889cd4436d56dedf64044d
childressdev <jcampbell@childressagency.com>
2017-09-14 17:02:53 -0400

    home demo styled

--- js/elkmobility-scripts.js
+++ js/elkmobility-scripts.js
@@ -1,18 +1,43 @@
 jQuery(document).ready(function($){
 
+  //hp-hero scroll down
+  $('#scrollDown').on('click', function (e) {
+    e.preventDefault;
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 57
+        }, 1000);
+        return false;
+      }
+    }
+  });  
+
   //testimonials slider
   if(typeof $.fn.lightSlider == 'function'){
-    $('#hp-testimonials').lightSlider({
+    var testimonialSlider = $('#testimonialSlider').lightSlider({
       item:1,
       loop:true,
       pager:false,
+      controls:false,
       gallery:false,
       auto:true,
       pauseOnHover:true,
       keyPress:true,
       adaptiveHeight:true,
-      //prevHtml:'',
-      //nextHtml:''
+      pause:4000,
+      prevHtml:'',
+      nextHtml:''
+    });
+    $('#goToPrevSlide').on('click', function(e){
+      e.preventDefault();
+      testimonialSlider.goToPrevSlide();
+    });
+    $('#goToNextSlide').on('click', function(e){
+      e.preventDefault();
+      testimonialSlider.goToNextSlide();
     });
   }
 
